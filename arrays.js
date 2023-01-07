4f1fdb4d5925fc0f1276fe276dcabab7d4aedc26
Davi Luiz <carlos.t.arruda@gmail.com>
2023-01-07 07:53:45 -0300

    Segundo .2 commit

---
+++ arrays.js
@@ -0,0 +1,27 @@
+let heroes = ['batman','catwoman','iron man']
+
+console.log(heroes)
+
+heroes[3] = 'Spiderman'
+
+console.log(heroes)
+
+heroes[0] = 'BobSponja'
+
+console.log(heroes)
+
+console.log(heroes.join())
+
+console.log(heroes.join('---'))
+
+console.log(heroes.indexOf('catwoman'))
+
+console.log(heroes.concat(['hombre raspadinha', 'melão das trevas']))
+
+console.log(heroes.push('he-man', 'Descarga fatal'))
+
+console.log(heroes)
+
+console.log(heroes.pop())
+
+console.log(heroes)
