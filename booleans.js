01965f5c4149f87b65d20d1adf9b8af22fa5f9a6
Davi Luiz <carlos.t.arruda@gmail.com>
2023-01-09 07:21:26 -0300

    Terceiro commit

---
+++ booleans.js
@@ -0,0 +1,15 @@
+const email = 'brucewayne@xxx.com'
+const include = email.includes('@')
+console.log(include)
+
+const names = ['carlos', 'elaine', 'davi']
+
+let arrayInclude = names.includes('elaine')
+console.log(arrayInclude)
+
+arrayInclude = names.includes('elai')
+console.log(arrayInclude)
+
+console.log('bela' > 'roger')
+console.log('bela' > 'Roger')
+console.log('bela' > 'Bela')
