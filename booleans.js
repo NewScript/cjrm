f4ba84bf61d526d382f8d08151423e409464f17b
Davi Luiz <carlos.t.arruda@gmail.com>
2023-01-13 07:00:20 -0300

    Converso de tipos

--- booleans.js
+++ booleans.js
@@ -1,15 +1,23 @@
-const email = 'brucewayne@xxx.com'
-const include = email.includes('@')
-console.log(include)
+// const email = 'brucewayne@xxx.com'
+// const include = email.includes('@')
+// console.log(include)
 
-const names = ['carlos', 'elaine', 'davi']
+// const names = ['carlos', 'elaine', 'davi']
 
-let arrayInclude = names.includes('elaine')
-console.log(arrayInclude)
+// let arrayInclude = names.includes('elaine')
+// console.log(arrayInclude)
 
-arrayInclude = names.includes('elai')
-console.log(arrayInclude)
+// arrayInclude = names.includes('elai')
+// console.log(arrayInclude)
 
-console.log('bela' > 'roger')
-console.log('bela' > 'Roger')
-console.log('bela' > 'Bela')
+// console.log('bela' > 'roger')
+// console.log('bela' > 'Roger')
+// console.log('bela' > 'Bela')
+
+const age = 31
+
+console.log(age == 31)
+
+console.log(age == '31')
+
+console.log(age === '31')
---
+++ conversaoDeTipos.js
@@ -0,0 +1,7 @@
+let score = '100'
+console.log(score + 1 + '\n')
+console.log(typeof score + '\n')
+
+score = Number(score)
+console.log(score + 1 + '\n')
+console.log(typeof score + '\n')
