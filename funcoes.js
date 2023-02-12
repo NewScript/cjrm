62f25e861ea790ade78e155682e7043bee578be5
Davi Luiz <carlos.t.arruda@gmail.com>
2023-02-12 10:17:04 -0300

    Um otimo exemplo de forEach

---
+++ funcoes.js
@@ -0,0 +1,63 @@
+const funcoes = [
+
+    function(){ return 'Ola'},
+
+    function(){
+        return 'Meu'
+    },
+
+    ()=>'Mundo'
+]
+
+let frase = ''
+
+for(let i = 0; i < funcoes.length; i++){
+    frase += `${funcoes[i]()} `
+}
+
+console.log(frase)
+
+
+const drobo1 = (number = 0) => {
+    return 2 * number
+}
+
+const drobo2 = number => {
+    return 2 * number
+}
+// Na versão acima não é possível
+// atribuição de parâmetro default
+
+let number =0
+const drobo3 = () => {
+    return 2 * number
+}
+
+const drobo4 = number => 2 * number
+
+//const drobo5 = number => if(2===2)console.log('Dois')
+// Desta forma não pode retornar uma expressão
+
+// Achei uma exceção
+const drobo5 = () => 3 > 4 ? 'sim':'não'
+
+console.log(drobo5())
+
+
+
+
+const myFunc = callback => {
+    const value = 77
+    callback( value )
+}
+
+myFunc( number => console.log(number) )
+
+
+const socialNetworks = [ 'youtube', 'twitter', 'instagram', 'facebook']
+
+const logArrayInfo = (socialNetwork, index, array) => {
+    console.log(`${index} - ${socialNetwork} - ${array}`)
+}
+
+socialNetworks.forEach( logArrayInfo )
