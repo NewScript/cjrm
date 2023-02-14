1418b82e5a3fb916b248d6f96091fada5a281b45
Davi Luiz <carlos.t.arruda@gmail.com>
2023-02-14 07:06:14 -0300

    Casos legais de objetos e arrays

---
+++ objetos.js
@@ -0,0 +1,57 @@
+let user = {
+    name: 'João',
+    idade: 48,
+    preferecncias: ['cha','café', 'cevada'],
+    exibir: ()=>console.log(user.name)
+}
+
+console.log(user)
+
+console.log(user.name)
+
+console.log(user.preferecncias[2])
+
+user.sexo = 'm'
+
+console.log(user)
+
+user.exibir();
+
+console.log(user['idade'])
+
+let show = {
+    exibir: function(){
+        console.log(user.name)
+    }
+}
+
+show.exibir()
+
+show.cabelo = ()=>console.log('preto')
+
+show.cabelo()
+
+//------------------------------------------------
+
+const letra = ['a','b','c','d']
+
+console.log(letra.includes('a'))
+console.log(letra.includes('f'))
+
+console.log(['a','b','c','d'].includes('a'))
+console.log(['a','b','c','d'].includes('f'))
+
+//------------------------------------------------
+
+const getTypeDefinition = type => {
+    const obj = {
+        null: 'Seta, explicitamente, uma variável sem valor',
+        undefined: 'Representa um valor não-setado (não atribuido)',
+        object: 'Arrays, Datas, Objetos literais, Funções, etc'
+    }
+    return obj[type]
+}
+
+console.log(getTypeDefinition('null'))
+console.log(getTypeDefinition('undefined'))
+console.log(getTypeDefinition('object'))
