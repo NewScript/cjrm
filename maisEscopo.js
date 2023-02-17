be448e97b51411536bf8450304cf7f7be64818a0
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2023-02-17 16:41:16 -0300

    encerrando o video mais Escopos

---
+++ maisEscopo.js
@@ -0,0 +1,16 @@
+const external = () => {
+    const book = 'Sapien'
+    const internal = () => {
+        //const book = 'Os testamentos'
+        const extraInternal = () => {
+            console.log( book. toUpperCase() )
+        }
+        //extraInternal()
+    }
+    internal()
+}
+
+
+
+
+
