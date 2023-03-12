4a35f7fb32ec2524f51821b5fc4c9743bb6c282f
Carlos Timoteo Arruda <carlos.t.arruda@gmail.com>
2023-03-12 18:10:06 -0300

    Metodo reduce

--- reduce.js
+++ reduce.js
@@ -1,3 +1,9 @@
+const numbers = [1, 2, 3]
+
+const sumResult = numbers.reduce((accumulator, item) => accumulator + item, 0)
+
+console.log(sumResult)
+
 const phaseScore = [
     { name: 'Carlos', score: 337 },
     { name: 'Davi', score: 234 },
